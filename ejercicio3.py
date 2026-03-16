2145c805c311a25b6a2efd7bb373d17645641f11
ANavarro96 <alejandronavarro96@hotmail.com>
2026-03-16 10:32:35 +0000

    Cleanup

--- ejercicio3.py
+++ ejercicio3.py
@@ -9,83 +9,8 @@ MODELO = "llama3.2:3b"
 Ejercicio 3 -> Analizador de sentimientos
 Haced que el modelo actúe como un analizador de sentimientos.
 Esto quiere decir que a vuestro prompt, tendría que concretar si es positivo, negativo, neutral....
-Estableced vosotros los valores posibles.
+Estableced vosotros las categorías posibles.
+Probad distintos modelos, ¿cuál es mejor, en vuestra opinión?
 """
 
-
-
-PROMPT_SISTEMA = "Eres un clasificador de sentimientos."
-
-prompt = """
-Clasifica el sentimiento de la siguiente frase.
-
-Frase:
-"Este curso de inteligencia artificial es increíble."
-
-Devuelve un JSON con esta estructura:
-
-{
- sentimiento: String
-}
-
-Valores posibles:
-
-positivo
-negativo
-neutral
-
-Devuelve SOLO el JSON.
-"""
-
-json_schema = {
-    "type": "object",
-    "properties": {
-        "sentimiento": {
-            "type": "string",
-            "enum": ["positivo", "negativo", "neutral"]
-        }
-    },
-    "required": ["sentimiento"]
-}
-
-VECES_INTENTADO = 1
-
-
-def comprobarSalida(salida):
-    try:
-        salidaJson = json.loads(salida)
-        print(f"Salida recibida: {salidaJson}")
-        validate(instance=salidaJson, schema=json_schema)
-    except Exception as e:
-        print("Salida incorrecta, reintentando...")
-        print(e)
-        return False
-
-    return True
-
-
-def hablarConChat(prompt):
-    global VECES_INTENTADO
-
-    response = chat(
-        model=MODELO,
-        messages=[
-            {"role": "system", "content": PROMPT_SISTEMA},
-            {"role": "user", "content": prompt},
-        ],
-    )
-
-    while not comprobarSalida(response["message"]["content"]):
-        VECES_INTENTADO += 1
-        prompt += f"\nLa salida no es correcta. Intento {VECES_INTENTADO}. Devuelve solo el JSON."
-
-        response = chat(
-            model=MODELO,
-            messages=[
-                {"role": "system", "content": PROMPT_SISTEMA},
-                {"role": "user", "content": prompt},
-            ],
-        )
-
-
-hablarConChat(prompt)
+PROMPT_SISTEMA = ""
